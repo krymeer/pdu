77422010c390529ab586f0c1f499ec7384285209
krymeer <krzysztof.radoslaw.osada@gmail.com>
2018-12-18 23:21:43 +0100

    Added button listeners for the Playground Details Screen

---
+++ place_zabaw/app/src/main/java/czernik/osada/placezabaw/CommentsScreen.java
@@ -0,0 +1,11 @@
+package czernik.osada.placezabaw;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CommentsScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- place_zabaw/app/src/main/java/czernik/osada/placezabaw/PlaygroundDetailsScreen.java
+++ place_zabaw/app/src/main/java/czernik/osada/placezabaw/PlaygroundDetailsScreen.java
@@ -1,12 +1,17 @@
 package czernik.osada.placezabaw;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class PlaygroundDetailsScreen extends AppCompatActivity{
+    private String address;
+    private float rating;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +21,14 @@ public class PlaygroundDetailsScreen extends AppCompatActivity{
         RatingBar ratingBar         = findViewById(R.id.playgroundDetails_rating);
         Intent intent               = getIntent();
         Bundle bundle               = intent.getExtras();
-        String address;
         String distance;
-        float rating;
 
-        if (bundle != null) {
+        if (bundle != null)
+        {
             if (intent.hasExtra("address"))
             {
-                address = bundle.getString("address");
-                addressTextView.setText(address);
+                this.address = bundle.getString("address");
+                addressTextView.setText(this.address);
             }
 
             if (intent.hasExtra("distance"))
@@ -35,9 +39,32 @@ public class PlaygroundDetailsScreen extends AppCompatActivity{
 
             if (intent.hasExtra("rating"))
             {
-                rating = bundle.getFloat("rating");
-                ratingBar.setRating(rating);
+                this.rating = bundle.getFloat("rating");
+                ratingBar.setRating(this.rating);
             }
         }
+    }
+
+    public void onRateBtnClick(View view)
+    {
+        Intent intent = new Intent(this, CommentsScreen.class);
+        intent.putExtra("rating", this.rating);
+        startActivity(intent);
+    }
+
+    public void onMapBtnClick(View view) {
+        Uri intentUri = Uri.parse("geo:0,0?q=" + Uri.encode(this.address));
+        Intent intent = new Intent(Intent.ACTION_VIEW, intentUri);
+        intent.setPackage("com.google.android.apps.maps");
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+    }
+
+    public void onReportBtnClick(View view) {
+        Intent intent = new Intent(this, ReportScreen.class);
+        startActivity(intent);
     }
 }
